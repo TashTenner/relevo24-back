e1df9edf9e4e38c1b1ad4b9a5fef7a934bc7890d
Tash Tenner <tashfonetikas@gmail.com>
2020-04-25 13:00:24 +0200

    update workingDay schema, test via Postman, routes work

--- routes/workingDay.js
+++ routes/workingDay.js
@@ -7,7 +7,7 @@ const WorkingDay = require('../models/WorkingDay');
 
 router.get('/', async (req, res, next) => {
   try {
-    const workingDays = await WorkingDay.find();
+    const workingDays = await WorkingDay.find().populate('employeesTeam').populate('shifts');
     res.json(workingDays);
   } catch (error) {
     next(error);
@@ -17,7 +17,7 @@ router.get('/', async (req, res, next) => {
 router.get('/:workingDayId', async (req, res, next) => {
   const { workingDayId } = req.params;
   try {
-    const workingDay = await WorkingDay.findById(workingDayId);
+    const workingDay = await WorkingDay.findById(workingDayId).populate('employeesTeam').populate('shifts');
     if (workingDay) {
       res.json(workingDay);
     } else {
@@ -28,13 +28,15 @@ router.get('/:workingDayId', async (req, res, next) => {
   }
 });
 
+// only needed to add the 7 days of the week
+
 router.post('/add', async (req, res, next) => {
   const {
-    day,
+    dayName, dayNumber,
   } = req.body;
   try {
     const workingDay = await WorkingDay.create({
-      day,
+      dayName, dayNumber,
     });
     res.json(workingDay);
   } catch (error) {
@@ -42,20 +44,24 @@ router.post('/add', async (req, res, next) => {
   }
 });
 
+// actually not needed
+
 router.put('/:workingDayId/update', async (req, res, next) => {
   const { workingDayId } = req.params;
   const {
-    day,
+    dayName, dayNumber,
   } = req.body;
   try {
     const workingDay = await WorkingDay.findByIdAndUpdate(workingDayId, { new: true }, {
-      day,
+      dayName, dayNumber,
     });
     res.json(workingDay);
   } catch (error) {
     next(error);
   }
 });
+
+// actually not needed
 
 router.delete('/:workingDayId/delete', async (req, res, next) => {
   const { workingDayId } = req.params;
